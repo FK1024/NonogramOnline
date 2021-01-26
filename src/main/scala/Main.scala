ad6498e15282018128e8e4df42cc51ade63a9390
Lukas Schondorff <l.schondorff@gmail.com>
2021-01-26 21:16:18 +0100

    added a debugtable + changed the button appearance on click

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,6 +5,7 @@ import org.scalajs.dom.raw.Element
 object Main {
   var id = 0
   var collection:Map[String,Int] = Map()
+  var gameboard:Array[Array[Int]] = Array()
 
   def main(args: Array[String]): Unit = {
     document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
@@ -16,18 +17,37 @@ object Main {
     createButton("Play Nonogram", true, createGame)
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    parNode.textContent = text
-    targetNode.appendChild(parNode)
-  }
-
   def createGame(): Unit = {
     removeElementByName("Play Nonogram")
-
     var createplayfield = new createplayfield(5,6)
-    document.body.appendChild(createplayfield.appendPar())
+    gameboard = createplayfield.initGameBoard()
+    document.body.appendChild(createplayfield.createPlayTable(buttonFunction))
+    document.body.appendChild(createplayfield.createDebugGameBoard(gameboard))
   }
+
+  def buttonFunction(button: Element, x: Int, y: Int): Unit ={
+    var s = " "
+    var n = 0
+
+    if (gameboard(y)(x) == 0) {
+      s = "X"
+      n = 1
+    } else if (gameboard(y)(x) == 1) {
+      s = "-"
+      n = 2
+    } else {
+      s = " "
+      n = 0
+    }
+
+    button.textContent = s
+    gameboard(y)(x) = n
+    editElementByName("d"+x+"|"+y, n.toString)
+
+  }
+
+  //--------------------------------------------------------------------------------
+  // Helper Functions: TODO auslagern ?
 
   def createButton(text: String, event: Boolean, eventfunc: () => Unit): Element = {
     val button = document.createElement("button")
@@ -37,19 +57,26 @@ object Main {
     id += 1
 
     if (event) {
-      button.addEventListener("click", {(e: dom.MouseEvent) =>
+      button.addEventListener("click", {e: dom.MouseEvent =>
         eventfunc()
       })
     }
     document.body.appendChild(button)
-
     return button
   }
 
   def removeElementByName(name: String): Unit = {
-    var elementid = collection.get(name).get.toString
-
-    println(document.getElementById(elementid).textContent)
+    var elementid = collection(name).toString
     document.body.removeChild(document.getElementById(elementid))
+  }
+
+  def editElementByName(name: String, newtext: String): Unit =  {
+    document.getElementById(name).textContent = newtext
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
   }
 }
--- src/main/scala/createplayfield.scala
+++ src/main/scala/createplayfield.scala
@@ -4,7 +4,21 @@ import org.scalajs.dom.raw.Element
 
 class createplayfield(x: Int, y: Int) {
 
-  def appendPar(): Element = {
+  def initGameBoard(): Array[Array[Int]] = {
+    var gameboard:Array[Array[Int]] = Array()
+
+    for(i <- 0 to y) {
+      var h:Array[Int] = Array()
+      for(j <- 0 to x) {
+        h = h :+ 0
+      }
+      gameboard = gameboard :+ h
+    }
+    return gameboard
+  }
+
+
+  def createPlayTable(buttonFunction: (Element, Int, Int) => Unit): Element = {
     var table = document.createElement("table")
     table.setAttribute("class","styled-table")
 
@@ -17,7 +31,7 @@ class createplayfield(x: Int, y: Int) {
         } else if(i==0) {
           content.textContent = j.toString
         } else {
-          content.appendChild(createTableButton(j,i))
+          content.appendChild(createTableButton(j,i, buttonFunction))
         }
         tablerow.appendChild(content)
       }
@@ -26,17 +40,40 @@ class createplayfield(x: Int, y: Int) {
     return table
   }
 
-  def createTableButton(x: Int, y: Int): Element = {
-    val button = document.createElement("button")
-    button.setAttribute("class","table-button")
-    button.textContent = x+"|"+y
-    button.addEventListener("click", {(e: dom.MouseEvent) =>
-      buttonFunction(x,y)
-    })
-    return button
+  // Only for Debug TODO remove when project is finished
+  def createDebugGameBoard(gameboard: Array[Array[Int]]): Element = {
+    var table = document.createElement("table")
+    table.setAttribute("class","styled-table")
+
+    for(i <- 0 to y) {
+      var tablerow = document.createElement("tr")
+      for(j <- 0 to x) {
+        var content = document.createElement("th")
+        if(j==0) {
+          content.textContent = i.toString
+        } else if(i==0) {
+          content.textContent = j.toString
+        } else {
+          if (j <= x || i <= y-1) {
+            content.textContent = gameboard(y)(x).toString
+            content.id = "d"+j+"|"+i
+          }
+        }
+        tablerow.appendChild(content)
+      }
+      table.appendChild(tablerow)
+    }
+    return table
   }
 
-  def buttonFunction(x: Int, y: Int): Unit ={
-    println(x+"|"+y)
+  def createTableButton(x: Int, y: Int, buttonFunction: (Element, Int, Int) => Unit): Element = {
+    val button = document.createElement("button")
+    button.setAttribute("class","table-button")
+    button.textContent = " "
+    button.id = x+"|"+y
+    button.addEventListener("click", {e: dom.MouseEvent =>
+      buttonFunction(button, x,y)
+    })
+    return button
   }
 }
